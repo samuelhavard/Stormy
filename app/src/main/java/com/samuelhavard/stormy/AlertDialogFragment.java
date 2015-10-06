ca28cd481972a71b58f38d5d5c3ee6c3ec80e4d1
Samuel Havard <SamuelHavard@yahoo.com>
2015-10-06 16:03:57 -0400

    updated error handling

---
+++ app/src/main/java/com/samuelhavard/stormy/AlertDialogFragment.java
@@ -0,0 +1,24 @@
+package com.samuelhavard.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by samue_000 on 10/6/2015.
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_ok_button, null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
--- app/src/main/java/com/samuelhavard/stormy/MainActivity.java
+++ app/src/main/java/com/samuelhavard/stormy/MainActivity.java
@@ -1,11 +1,15 @@
 package com.samuelhavard.stormy;
 
 import android.app.DownloadManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -30,30 +34,54 @@ public class MainActivity extends AppCompatActivity {
         String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
                 "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(forecastUrl)
-                .build();
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
+        if (isNetworkAvilable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
 
-            }
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
 
-            @Override
-            public void onResponse(Response response) throws IOException {
-                try {
-                    if (response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "Exception caught", e);
                 }
-            }
-        });
 
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught", e);
+                    }
+                }
+            });
+        } else  {
+            Toast.makeText(this, getString(R.string.network_unavailable_string),
+                    Toast.LENGTH_LONG).show();
+        }
         Log.d(TAG, "Main UI code is running.");
+    }
+
+    private boolean isNetworkAvilable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
