9422f05ea8e514a14eab4578fead85735380ae76
Samuel Havard <SamuelHavard@yahoo.com>
2015-10-12 17:21:36 -0400

    updated view

--- app/src/main/java/com/samuelhavard/stormy/MainActivity.java
+++ app/src/main/java/com/samuelhavard/stormy/MainActivity.java
@@ -56,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
                         String jsonData = response.body().string();
                         if (response.isSuccessful()) {
                             mCurrentWeather = getCurrentDetails(jsonData);
-
                         } else {
                             alertUserAboutError();
                         }
